3b0578e18872ea01803dbf1d7e469c549a3746c8
NickChrys <nchrysanthopoulos18@gmail.com>
2025-02-25 17:18:02 -0500

    Flight Itinerary Style

--- PoseidonDesignSys/lib/components/PFlight.vue
+++ PoseidonDesignSys/lib/components/PFlight.vue
@@ -51,6 +51,10 @@ const props = defineProps({
         type: String,
         default: '00'
     },
+    flightNumber: {
+        type: String,
+        default: '0000'
+    },
     airline: {
         type: String,
         default: 'Airline'
@@ -66,10 +70,10 @@ const props = defineProps({
 });
 
 const base = 'p-flight';
-  const giveDesign = computed(() => {
+const giveDesign = computed(() => {
     const design = props.design !== 'default' ? `${base}--${props.design}` : base;
     return [design]
-  });
+});
 
 const getOrdIndicator = (day) => {
     if (day > 3 && day < 21) return 'th';
@@ -104,29 +108,73 @@ const formatFlightDate = computed(() => {
 <template>
 
     <div :class="giveDesign" @click="$emit('click')">
-        <div class="p-flight__airline">
-            <img class="p-flight__airline-logo" :src="logoURL" :alt="`${airline} Logo`"/>
-            <h5 class="p-flight__airline-text">{{ airline }}</h5>
-        </div>
-        <div class="p-flight__info">
-            <div class="date-time-container">
-                <h5 class="p-flight__date">{{ formatFlightDate }}</h5>
-                <h5 class="p-flight__time">{{ formatTime(flightDepTime) }} - {{ formatTime(flightArrTime)
-                    }}
-                </h5>
+
+        <template v-if="giveDesign.includes('p-flight--itinerary')">
+            <div class>
+                <h3>{{ flightDate }}</h3>
+                <div class="p-flight--itinerary__container">
+                    <div class="p-flight--itinerary__time-place">
+                        <div class="p-flight--itinerary__depart-container">
+                            <div class="p-flight--itinerary__place">
+                                <h3>Departure</h3>
+                                <h2>{{ origin }}</h2>
+                                <h3>City, State</h3>
+                            </div>
+                            <div class="p-flight--itinerary__time">
+                                <h3>{{ formatTime(flightDepTime) }}</h3>
+                            </div>
+                        </div>
+                        <div class="p-flight--itinerary__arrow">
+                            <p>(Calculated time)</p>
+                        </div>
+
+                        <div class="p-flight--itinerary__arrive-container">
+                            <div class="p-flight--itinerary__place">
+                                <h3>Arrival</h3>
+                                <h2>{{ destination }}</h2>
+                                <h3>City, State</h3>
+                            </div>
+                            <div class="p-flight--itinerary__time">
+                                <h3>{{ formatTime(flightArrTime) }}</h3>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="p-flight--itinerary__info">
+                        <h3>{{ flightNumber }}</h3>
+                        <h3>{{ flightType }}</h3>
+                        <h3>{{ flightClass }}</h3>
+                        <h3>{{ seatNumber }}</h3>
+                    </div>
+                </div>
             </div>
-            <div class="p-flight__details">
-                <h5>{{ origin }}-{{ destination }}</h5>
-                <h5>{{ flightType }}</h5>
-                <h5 v-if="giveDesign.includes('p-flight--block')">Seat {{ seatNumber }}</h5>
-                <h5 v-if="giveDesign.includes('p-flight--block')">Gate {{ flightGate }}</h5>
-                <h5 v-if="giveDesign.includes('p-flight--shop')">{{ seatAvailable }} Available Seats</h5>
+        </template>
+
+        <template v-else>
+            <div class="p-flight__airline">
+                <img class="p-flight__airline-logo" :src="logoURL" :alt="`${airline} Logo`" />
+                <h5 class="p-flight__airline-text">{{ airline }}</h5>
             </div>
-        </div>
-        <div class="class-price-container">
-            <h5 class="p-flight__class">{{ flightClass }}</h5>
-            <h5 class="p-flight__price">${{ price }}</h5>
-        </div>
+            <div class="p-flight__info">
+                <div class="date-time-container">
+                    <h5 class="p-flight__date">{{ formatFlightDate }}</h5>
+                    <h5 class="p-flight__time">{{ formatTime(flightDepTime) }} - {{ formatTime(flightArrTime)
+                        }}
+                    </h5>
+                </div>
+                <div class="p-flight__details">
+                    <h5>{{ origin }}-{{ destination }}</h5>
+                    <h5>{{ flightType }}</h5>
+                    <h5 v-if="giveDesign.includes('p-flight--block')">Seat {{ seatNumber }}</h5>
+                    <h5 v-if="giveDesign.includes('p-flight--block')">Gate {{ flightGate }}</h5>
+                    <h5 v-if="giveDesign.includes('p-flight--shop')">{{ seatAvailable }} Available Seats</h5>
+                </div>
+            </div>
+            <div class="class-price-container">
+                <h5 class="p-flight__class">{{ flightClass }}</h5>
+                <h5 class="p-flight__price">${{ price }}</h5>
+            </div>
+        </template>
     </div>
 
 </template>
